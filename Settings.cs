8769d7f7977805d501d119fa974856d1c9559467
GITHAE KEVIN <Kevogich@live.com>
2022-10-20 14:15:47 +0200

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -5,6 +5,7 @@
     //  The SettingChanging event is raised before a setting's value is changed.
     //  The PropertyChanged event is raised after a setting's value is changed.
     //  The SettingsLoaded event is raised after the setting values are loaded.
+    // any other additional settings 
     //  The SettingsSaving event is raised before the setting values are saved.
     internal sealed partial class Settings {
         
